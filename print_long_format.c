80718aa813861c07da976abd0db9bab98f7a3925
Yevhenii MARCHYSHYN <ymarchys@e1r3p5.unit.ua>
2018-02-23 13:46:19 +0200

    save

--- print_long_format.c
+++ print_long_format.c
@@ -12,6 +12,14 @@
 
 #include "ft_ls.h"
 
+void		device_number(struct stat *buf)
+{
+	ft_printf("%lld", buf->st_rdev >> 24 & 0xff);
+	ft_printf(",    ");
+	ft_printf("%lld", buf->st_rdev & 0xffffff);
+	ft_printf(" ");
+}
+
 void		print_long_format(char *filename, int *w, char *path)
 {
 	struct stat		buf;
@@ -35,8 +43,11 @@ void		print_long_format(char *filename, int *w, char *path)
 	print_access(buf.st_mode);
 	ft_printf(" %*zu", w[0] + 1, buf.st_nlink);
 	ft_printf(" %-*s ", w[1] + 1, pw->pw_name);
-	ft_printf(" %*s ", w[2] + 1, gr->gr_name);
-	ft_printf("%*zu", w[3] + 1, buf.st_size);
+	ft_printf("%-*s ", w[2] + 1, gr->gr_name);
+	if (define_type(&buf) == 'c' || define_type(&buf) == 'b')
+		device_number(&buf);
+	else
+		ft_printf("%*zu", w[3] + 1, buf.st_size);
 	ft_printf(" %s", time_format(buf.st_mtime));
 	if (define_type(&buf) == 'l')
 	{
